57517d3f8d45cc8c43d8a3dfe865668b17aa37cc
aymeric3960 <pk38ubw@eduvaud.ch>
2023-11-29 19:03:28 +0100

    ajout solution corr test

---
+++ ACL/testQ4.h
@@ -0,0 +1,13 @@
+//#pragma once
+#ifndef testQ4
+#define testQ4
+
+#include<stdint.h>
+
+const uint16_t VALDS = 0xFF33;
+const int SIGNE = 0xFF;
+#define ENT 0x00FF
+#define DEM 0x0001
+#define HALF 0.5
+#define EIGHT 8
+#endif
